a9ea25d9fbecdfda0751c45c586ddfae9d43ec87
Amarjith <72960252+amarjithp@users.noreply.github.com>
2023-10-24 17:00:12 +0530

    body/ sidebar

--- app/components/Body.tsx
+++ app/components/Body.tsx
@@ -4,8 +4,8 @@ function Body() {
   return (
     // body
     <div className='flex m-5'>
-        <h1 className='w-10/12 bg-red-400'>s</h1>
-        <h1 className='w-2/12 ml-5 bg-green-400'>sdfds</h1>
+        <h1 className='w-10/12 bg-red-400'>main body</h1>
+        <h1 className='w-2/12 ml-5 bg-green-400'>sidebar</h1>
     </div>
     // sidebar
   )

474d42256079338163f363ceb3363a0b12ae7ffd
Amarjith <72960252+amarjithp@users.noreply.github.com>
2023-10-29 20:57:21 +0530

    better position of icons in discover

--- app/components/MainBody.tsx
+++ app/components/MainBody.tsx
@@ -25,47 +25,56 @@ function MainBody() {
                 just a few people <br></br> who create elegant<br></br>
                 crafts & jewellery</p>
                 <div className='flex mt-8'>
-                    <svg className='w-10 h-10 mr-4 bg-green-400 rounded-full p-1' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                    <svg className='w-10 h-10 mr-4 bg-headopt rounded-full p-1' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
                     </svg>
 
                     
                     <MdDiamond className='w-10 h-10
-                    bg-green-400 
+                    bg-headopt 
                     rounded-full p-1 justify-center items-center'
                     />
                     
 
-                    <div className='w-32 ml-4 rounded-full flex justify-center items-center bg-yellow-400'>
-                        <p>Products</p>
+                    <div className='w-32 ml-4 
+                    rounded-full flex 
+                    justify-center items-center 
+                    bg-gray-400'>
+                        <p className='text-black'>Products</p>
                     </div>
 
                 </div>
 
-                <div className='mt-4 lg:h-60 '>
+                <div className='mt-4 lg:h-60'>
                     <h2 className='font-Poppins font-bold text-lg'>Discover</h2>
                     <Image
-                        className='w-full h-fit lg:w-8/12 lg:h-2/3'
+                        className='w-full h-fit mt-2 lg:w-8/12 lg:h-2/3'
                         src="https://pyxis.nymag.com/v1/imgs/0e8/abc/4c1808fa323cce7b95569da349dff28654-ana-de-armas.1x.rsocial.w1200.jpg"
                         alt='Discover'
                         width={300}
                         height={100}
                     />
 
-                    <div className='flex'>
-                        <p className='mt-4 ml-0'>Ana de Armas</p>
+                    <div className='flex w-full h-fit 
+                    items-center 
+                    lg:w-8/12 mt-4
+                    justify-between'
+                    >
+                        <p className=''>Ana de Armas</p>
+                        <div className='flex'>
+                          <AiOutlineLeft className='w-8 h-8 
+                          p-1 
+                          bg-headopt 
+                          rounded-full'
+                          />
 
-                        <AiOutlineLeft className='w-8 h-8 
-                        mt-3 ml-36 p-1 
-                        bg-headopt 
-                        rounded-full'
-                        />
+                          <AiOutlineRight className='w-8 h-8 
+                          ml-4 p-1 
+                          bg-headopt 
+                          rounded-full'
+                          />
+                        </div>
 
-                        <AiOutlineRight className='w-8 h-8 
-                        mt-3 ml-4 p-1 
-                        bg-headopt 
-                        rounded-full'
-                        />
                     </div>
 
                 </div>
